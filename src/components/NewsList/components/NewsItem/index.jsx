f23351cdecc2208c0315d7f763b201acb0522e4e
pticasolovey20 <dy.yev.solovyov@gmail.com>
2022-12-19 12:26:20 +0200

    added responsive news items

--- src/components/NewsList/components/NewsItem/index.jsx
+++ src/components/NewsList/components/NewsItem/index.jsx
@@ -8,15 +8,15 @@ import { prepareText } from '../../../utils/prepareText';
 import styles from './NewsItem.module.scss';
 
 const NewsItem = ({ item }) => {
-    return (
-        <Link key={item.id} to={routeNewsDetail(item.id)}>
-            <div className={styles.newsItem}>
-                <div className={styles.title}>{prepareText(item.title)}</div>
-                <img src={item.imgUrl} alt={item.imgUrl} />
-                <DateView pubDate={item.pubDate} />
-            </div>
-        </Link>
-    );
+	return (
+		<Link key={item.id} to={routeNewsDetail(item.id)}>
+			<div className={styles.newsItem}>
+				<div className={styles.title}>{prepareText(item.title)}</div>
+				<img src={item.imgUrl} alt={item.imgUrl} />
+				<DateView pubDate={item.pubDate} />
+			</div>
+		</Link>
+	);
 };
 
 export { NewsItem };
--- src/components/NewsList/index.jsx
+++ src/components/NewsList/index.jsx
@@ -5,13 +5,13 @@ import { NewsItem } from './components/NewsItem';
 import styles from './NewsList.module.scss';
 
 const NewsList = ({ list }) => {
-    return (
-        <div className={styles.newsList}>
-            {list.map(news => (
-                <NewsItem key={news.id} item={news} />
-            ))}
-        </div>
-    );
+	return (
+		<div className={styles.newsList}>
+			{list.map(news => (
+				<NewsItem key={news.id} item={news} />
+			))}
+		</div>
+	);
 };
 
 export { NewsList };
